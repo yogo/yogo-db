52f6feb1cbc21f88330af177272394dcc3d75882
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-14 10:21:03 -0600

    Setting up rspec to run the tests in our rack environmnet

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,27 @@
+require 'rubygems'
+
+begin
+  require 'bundler'
+  Bundler.setup
+rescue LoadError
+  puts "Bundler is not intalled. Install with: gem install bundler"
+end
+
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'yogo-db'
+require 'yogo/db'
 require 'spec'
 require 'spec/autorun'
+require 'rack/test'
+require 'rack/mock'
+
+begin
+  require 'ruby-debug'
+rescue Exception => e
+  puts "ruby-debug is not installed. Install it with 'gem install ruby-debug'"
+end
+
 
 Spec::Runner.configure do |config|
-  
+  config.include Rack::Test::Methods
 end
