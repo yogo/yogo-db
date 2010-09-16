4f38dedf90d126567cc1d12a8f71f0a328b7103c
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-16 13:04:44 -0600

    Updating model_lookup_spec

--- spec/yogo/model_lookup_spec.rb
+++ spec/yogo/model_lookup_spec.rb
@@ -2,19 +2,43 @@
 require File.expand_path(File.dirname(__FILE__) + '/..' + '/spec_helper')
 
 # Testing the middleware and how it modifies the environment
-describe "Schema Middleware" do
+describe "Model lookup" do
     
-  describe 'yogo.resource' do
+  describe 'with an existing configuration' do
     
-    it "should be a key in the environment hash" do
-      env = env_with_params('/schema/some_id')
+    before(:all) do
+      @configuration = Schema.new(:name => 'sample_id')
+      @configuration.operation('add/property', :id, 'Serial')
+      @configuration.operation('add/property', :name, 'String')
+      @configuration.save
+    end
+    
+    after(:all) do
+      @configuration.destroy
+    end
+    
+    it "should add a yogo.resource key to the environment hash" do
+      env = env_with_params('/schema/sample_id')
       response = setup_rack().call(env)
       env.should be_a Hash
       env.should have_key('yogo.resource')
     end
 
+    it "should add a yogo.schema key to the environment hash" do
+      env = env_with_params('/schema/sample_id')
+      response = setup_rack().call(env)
+      env.should be_a Hash
+      env.should have_key('yogo.schema')
+    end
+
     it "should point to a datamapper model" do
-      env = env_with_params('/schema/some_id')
+      env = env_with_params('/schema/sample_id')
+      response = setup_rack.call(env)
+      env['yogo.resource'].should be_kind_of(DataMapper::Model)
+    end
+    
+    it "should set a model when more then a path was specified" do
+      env = env_with_params('/schema/sample_id/more_path')
       response = setup_rack.call(env)
       env['yogo.resource'].should be_kind_of(DataMapper::Model)
     end
@@ -35,10 +59,11 @@ describe "Schema Middleware" do
     }.should_not change(env, :keys)
   end
   
-  it "should set a model when more then a path was specified" do
-    env = env_with_params('/schema/model_id/more_path')
-    response = setup_rack.call(env)
-    env['yogo.resource'].should be_kind_of(DataMapper::Model)
+  it "should not set a model if a model id was specified but doesn't exist" do
+    env = env_with_params('/schema/some%20sort%20of%20model')
+    setup_rack.call(env)
+    env['yogo.resource'].should be_nil
+    env['yogo.schema'].should be_nil
   end
   
 end
