db028c579d2a07d4f88e14357105770bdd5cd7f9
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-16 13:01:57 -0600

    Adding config/requires to handle necessary requires Updating spec_helper to use requires

---
+++ config/requires.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+require 'rubygems'
+require 'bundler'
+
+Bundler.require(:default, ENV['RACK_ENV'] || :development )
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app', 'model'))
+
+
+require 'config/datamapper'
+require 'yogo/rack/model_lookup'
+require 'yogo/schema_app'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
-require 'rubygems'
+# require 'rubygems'
+# 
+# begin
+#   require 'bundler'
+#   Bundler.setup
+# rescue LoadError
+#   puts "Bundler is not intalled. Install with: gem install bundler"
+# end
+# 
+# $LOAD_PATH.unshift(File.dirname(__FILE__))
+# $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-begin
-  require 'bundler'
-  Bundler.setup
-rescue LoadError
-  puts "Bundler is not intalled. Install with: gem install bundler"
-end
+require './config/requires'
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'app'))
-
-require 'yogo/model_lookup'
 require 'rspec/core'
 require 'autotest/rspec2'
 require 'rack/test'
@@ -22,7 +22,6 @@ begin
 rescue Exception => e
   puts "ruby-debug is not installed. Install it with 'gem install ruby-debug'"
 end
-
 
 Dir[File.join(File.dirname(__FILE__), "helpers", "**/*.rb")].each do |f|
   require f
