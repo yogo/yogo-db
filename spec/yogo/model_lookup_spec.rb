feb5842c20e456cdd504c316584920ce74e89599
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-21 11:28:29 -0600

    Updating spec tests

--- spec/yogo/model_lookup_spec.rb
+++ spec/yogo/model_lookup_spec.rb
@@ -3,7 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/..' + '/spec_helper')
 
 # Testing the middleware and how it modifies the environment
 describe "Model lookup" do
-    
+  include Rack::Test::Methods
+  
+  def app  
+    setup_rack() 
+  end
+  
   describe 'with an existing configuration' do
     
     before(:all) do
@@ -19,27 +24,27 @@ describe "Model lookup" do
     
     it "should add a yogo.resource key to the environment hash" do
       env = env_with_params('/schema/sample_id')
-      response = setup_rack().call(env)
+      response = app.call(env)
       env.should be_a Hash
       env.should have_key('yogo.resource')
     end
 
     it "should add a yogo.schema key to the environment hash" do
       env = env_with_params('/schema/sample_id')
-      response = setup_rack().call(env)
+      response = app().call(env)
       env.should be_a Hash
       env.should have_key('yogo.schema')
     end
 
     it "should point to a datamapper model" do
       env = env_with_params('/schema/sample_id')
-      response = setup_rack.call(env)
+      response = app.call(env)
       env['yogo.resource'].should be_kind_of(DataMapper::Model)
     end
     
     it "should set a model when more then a path was specified" do
       env = env_with_params('/schema/sample_id/more_path')
-      response = setup_rack.call(env)
+      response = app.call(env)
       env['yogo.resource'].should be_kind_of(DataMapper::Model)
     end
   end
@@ -47,7 +52,7 @@ describe "Model lookup" do
   it "should not change the keys environment without a proper URI path" do
     env = env_with_params('/')
     lambda {
-      setup_rack.call(env)
+      app.call(env)
     }.should_not change(env, :keys)
   end
   
@@ -55,15 +60,21 @@ describe "Model lookup" do
   it "should not set a model if no model id was specified" do
     env = env_with_params('/schema')
     lambda {
-      setup_rack.call(env)
+      app.call(env)
     }.should_not change(env, :keys)
   end
   
   it "should not set a model if a model id was specified but doesn't exist" do
     env = env_with_params('/schema/some%20sort%20of%20model')
-    setup_rack.call(env)
+    app.call(env)
     env['yogo.resource'].should be_nil
     env['yogo.schema'].should be_nil
   end
   
+  # This test feels funny here.
+  it "should respond with a 404 if a model id was specified but doesn't exist" do
+    get '/schema/with_bad_id'
+    last_response.should_not be_ok
+    last_response.status.should eql(404)
+  end
 end
--- spec/yogo/schema_app_spec.rb
+++ spec/yogo/schema_app_spec.rb
@@ -22,12 +22,31 @@ describe Yogo::SchemaApp do
   end
   
   describe "when retriving /schema/:id" do
-    it "should retrieve retrieve a particular schema"
-    it "should give a 404 error when the schema does not exist"
+    it "should retrieve retrieve a particular schema" do
+      schema = Factory.create(:schema)
+      get "/schema/#{schema.name}"
+      last_response.should be_ok
+      last_response.body.should eql schema.to_json
+    end
+    
+    it "should give a 404 error when the schema does not exist" do
+      get '/schema/with_bad_id'
+      last_response.should_not be_ok
+      last_response.status.should eql(404)
+    end
   end
   
   describe "when posting to /schema" do
-    it "should create a new schema with the provided id" 
+    it "should create a new schema with the provided id" do
+      post '/schema', {:name => 'test_item', 
+        :op_defs => [['add/property', :id, 'Serial'],['add/property', :name, 'String']]}.to_json
+
+      last_response.status.should eql(201)
+      
+      get '/schema/test_item'
+      last_response.should be_ok
+    end
+    
     it "should not create a new schema with invalid data"
   end
   
