2f75cefaa74fdda31abeec604c80ec5626d54596
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-21 11:22:51 -0600

    Have datamapper.rb load a database based on the current environment

--- config/datamapper.rb
+++ config/datamapper.rb
@@ -4,4 +4,4 @@
 
 databases = YAML.load_file(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml'))
 
-DataMapper.setup(:default, databases['development'])
+DataMapper.setup(:default, databases[ENV['RACK_ENV']])
