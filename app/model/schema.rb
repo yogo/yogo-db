31aa5734e68cc2672141306623a8727fe077da96
Robbie Lamb <robbie.lamb@gmail.com>
2010-09-24 18:38:51 -0600

    Fixing issue where a resorce isn't marked dirty when updating attributes.

--- app/model/schema.rb
+++ app/model/schema.rb
@@ -3,7 +3,7 @@ require 'yogo/datamapper/model/configuration'
 
 class Schema
   include ::DataMapper::Resource
-  include Yogo::DataMapper::Model::Configuration
+  # include Yogo::DataMapper::Model::Configuration
   include Dataflow
   
   # Common::Properties::UUIDKey[self]
@@ -11,29 +11,35 @@ class Schema
 
   property :name,                       String,     :required => true, :key => true, :unique => true
   
-  property :op_defs,                    Yaml,       :lazy => false, :default => []
+  property :operation_definitions,      Yaml,       :lazy => false, :default => []
   
-  before :save, :set_op_defs
+  before :save, :reset_data_model
   before :destroy, :destroy_data_model_bucket
   
   # How do we know what variables need to be constantized.
   # Also, we might have more or fewer then 3 parameters in the operation
-  def operation_definitions
-    @__operation_definitions ||= attribute_get(:op_defs)
-  end
+  # def operation_definitions
+  #   @__operation_definitions ||= attribute_get(:op_defs)
+  # end
   
   # Set only the operations we want, don't add to them
   def operations=(ops)
     # Clear the current operations
-    @__operation_definitions = []
+    self.operation_definitions = []
     # Load operations with the set given
     ops.each{|op| operation(*op) }
   end
   
+  def operation(op_name, *args)
+    op_def = [op_name.to_s, args].flatten
+    unless self.operation_definitions.include?(op_def)
+      # We need to dup this else the model doesn't get marked as dirty and won't save.
+      self.operation_definitions =  self.operation_definitions.dup << op_def
+    end
+  end
+  
   def data_model
-    # @data_model ||= by_need { gen_model }
-     # by_need { gen_model }
-    @data_model ||= gen_model
+    @data_model ||= by_need { gen_model }
   end
   
   def to_url
@@ -61,6 +67,17 @@ class Schema
     ret
   end
   
+  def to_proc
+    ops = operation_definitions.map{|op_def| Yogo::DataMapper::Model::Operations[op_def.first] }
+    partial_ops = []
+    ops.each_with_index do |op, i|
+      next unless op
+      partial_ops[i] = op.partial(X, *operation_definitions[i][1..-1])
+    end
+    partial_ops.compact!
+    partial_ops.reduce{|composed, partial_op| composed * partial_op}
+  end
+  
   private
   
   def gen_model
@@ -81,8 +98,7 @@ class Schema
     return self.to_proc[base_model]
   end
   
-  def set_op_defs
-    attribute_set(:op_defs, operation_definitions)
+  def reset_data_model
     @data_model = nil
   end
   
