da3125dc9a1439dc903ba378feb6713412a2ef1e
Robbie Lamb <robbie.lamb@gmail.com>
2010-10-04 13:45:54 -0600

    Adding a Sinatra data endpoint

---
+++ spec/yogo/data_app_spec.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+require File.expand_path(File.dirname(__FILE__) + '/..' + '/spec_helper')
+
+require 'yogo/data_app'
+
+# Testing potential endpoints
+describe Yogo::SchemaApp do
+  include Rack::Test::Methods
+  
+  def app
+    setup_rack(Yogo::DataApp)
+  end
+  
+  before(:all) do
+    @cur_schema = Factory.create(:schema)
+  end
+  
+  after(:all) do
+    @cur_schema.destroy
+  end
+  
+  describe "when retrieving /data/:model_id" do
+  
+    it "should return all data" do
+      get "/data/#{@cur_schema.name}", :headers => {'accepts' => 'application/json'}
+
+      last_response.should be_ok
+      last_response.headers['content-type'].should eql 'application/json'
+    end
+  end
+  
+end
